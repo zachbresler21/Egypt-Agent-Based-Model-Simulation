181aa3855b1b39d3f700dce6cdd99eedb93b4e5a
zachbresler21 <49684878+zachbresler21@users.noreply.github.com>
2019-08-08 13:11:28 +0200

    Update Settlement

--- Settlement.py
+++ Settlement.py
@@ -1,32 +1,29 @@
+import numpy as np
 class Settlement:
 	#test
 	#Attributes
 	__settlement_id = 0
-	__households = ["hdfg","sff","syet"] #list of household objects belonging to this settlement
-	__population = 30
-	__num_households = 3
-	__grain = 4000
-	__neighbors = [] # list of settlement objects
+	__household_List= np.empty(250, dtype= object) #list of household objects belonging to this settlement
+	__population = 0
+	__num_households = 0
+	__grain = 0
+	#__neighbors = [] # list of settlement objects
 	__colour = "white"
 	__coordinates = [] #list of x and y coordinates of the position of the settlements
 
 	"""docstring for Settlement"""
-	def __init__(self, __settlement_id, __households, __population, __num_households, __grain, __neighbors, __colour, __coordinates):
-		#super(Settlement, self).__init__()
+	def __init__(self, __settlement_id, __households, __population, __num_households, __grain, __colour):
 		self.__settlement_id = __settlement_id
 		self.__households = __households
 		self.__population = __population
 		self.__num_households = __num_households
 		self.__grain = __grain
-		self.__neighbors = __neighbors
 		self.__colour = __colour
-		self.__coordinates = __coordinates
 
 
 	def checkSettlementPopulation(self):
 		#Size 1 - small ; 2 - medium ; 3 large ; 4 XL
 		#Same as setSize() method that used to be in this class
-		s = Settlement(self)
 		if self.__population <= 0:
 			#removeSettlement
 			return 0
@@ -40,18 +37,22 @@ class Settlement:
 			return 1
 
 	def addHousehold(self, household):
-=======
+		pass
+
 	def setHouseholds(self, householdList):
 		#Called from setUpSettlements in simulate class - sets the households in each settlement
 		self.__settlement_id = __settlement_id
 		__settlement_id += 1
 		self.__households = householdList
-
-
 		#takes in either a list of household objects or just one household object
 		#adds/removes a household for populationShift() OR fission()
 		self.__households.append(household)
 
+	def setSize():
+		pass
+
+	def setCoordinates(self, coords):
+		self.__coordinates = coords
 
 	def removeHousehold(self, household):
 		count = 0
