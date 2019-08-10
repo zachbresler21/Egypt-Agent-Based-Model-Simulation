3566ea926d94d1a91570eaff8320955320adef08
zachbresler21 <49684878+zachbresler21@users.noreply.github.com>
2019-08-10 10:55:57 +0200

    update

--- Settlement.py
+++ Settlement.py
@@ -1,9 +1,11 @@
 import numpy as np
+from Household import Household
+from Patch import Patch
 class Settlement:
 	#test
 	#Attributes
 	__settlement_id = 0
-	__household_List= np.empty(250, dtype= object) #list of household objects belonging to this settlement
+	__household_List= np.empty(250, dtype= Household) #list of household objects belonging to this settlement
 	__population = 0
 	__num_households = 0
 	__grain = 0
@@ -12,14 +14,17 @@ class Settlement:
 	__coordinates = [] #list of x and y coordinates of the position of the settlements
 
 	"""docstring for Settlement"""
-	def __init__(self, __settlement_id, __households, __population, __num_households, __grain, __colour):
+	def __init__(self, __settlement_id, __population, __num_households, __grain, __colour):
 		self.__settlement_id = __settlement_id
-		self.__households = __households
 		self.__population = __population
 		self.__num_households = __num_households
 		self.__grain = __grain
 		self.__colour = __colour
 
+	def __init__(self, __settlement_id, __population, __num_households):
+		self.__settlement_id = __settlement_id
+		self.__population = __population
+		self.__num_households = __num_households
 
 	def checkSettlementPopulation(self):
 		#Size 1 - small ; 2 - medium ; 3 large ; 4 XL
@@ -41,12 +46,12 @@ class Settlement:
 
 	def setHouseholds(self, householdList):
 		#Called from setUpSettlements in simulate class - sets the households in each settlement
-		self.__settlement_id = __settlement_id
-		__settlement_id += 1
-		self.__households = householdList
+		#self.__settlement_id = __settlement_id
+		#__settlement_id += 1
+		self.__household_List = householdList
 		#takes in either a list of household objects or just one household object
 		#adds/removes a household for populationShift() OR fission()
-		self.__households.append(household)
+		#self.__households.append(household)
 
 	def setSize():
 		pass
