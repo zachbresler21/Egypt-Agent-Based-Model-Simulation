8d455b2116e9dc1a893f10820ce5ce430515a1d3
zachbresler21 <49684878+zachbresler21@users.noreply.github.com>
2019-09-04 22:41:50 +0200

    update flood
    -calls the gridRecolor method in map to edit the grid for recolouring

--- Flood.py
+++ Flood.py
@@ -1,25 +1,28 @@
+from Map import Map
+from Patch import Patch
+import random
+import math
+
+
 class Flood:
 	#Atributes
-	
+	map = Map()
 
 	"""docstring for Flood"""
-	def __init__(self, arg):
-		super(Flood, self).__init__()
-		self.arg = arg
-		__fertility_colour = "White"
-	
-	def beginFlood():
-		#
-		pass
+	def __init__(self):
+		self.__fertility_colour = "White"
 
-	def setFertility():
-		#
-		pass
+	def setFertility(self):
+		patches = self.map.getPatches()
+		mu = random.randint(0,10) + 5
+		sigma = random.randint(0,5) + 5
+		alpha = 2 * math.pow(sigma,2)
+		beta = 1 / (sigma * math.sqrt(2 * math.pi))
 
-	def assignFertilityColour():
-		#
-		pass
-
-	def fieldsAffected():
-		#returns a list of Field objects
-		pass
+		for i in range (41):
+			for j in range(41):
+				fertility = 17 * (beta * (math.exp(0 - math.pow((patches[i][j].findCoordinates()[0] - mu),2)/alpha)))
+				if(patches[i][j].isField()):
+					patches[i][j].inner.setFertility(fertility)
+					patches[i][j].inner.setHarvestFalse()
+		self.map.gridRecolour()
