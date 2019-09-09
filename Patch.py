1e8d711498b4ca8fbea6630d9b301e6e39853494
Chadpiha <48052303+Chadpiha@users.noreply.github.com>
2019-09-09 20:45:38 +0200

    added comments

--- Patch.py
+++ Patch.py
@@ -2,7 +2,9 @@ import math
 class Patch:
 
 	#Attributes
-
+	'''
+	Constructor method
+	'''
 	def __init__(self, patch_id, isField):
 		self.__isSettlement = False
 		self.__isRiver = False
@@ -13,27 +15,48 @@ class Patch:
 		if self.__isField == True:
 			self.inner = self.Field(patch_id)
 
-
+	'''
+	Gets the ID of the patch
+	'''
 	def getID(self):
 		return self.__patch_id
 
+	'''
+	Find the coordinates of the patch
+	'''
 	def findCoordinates(self):
 		r = math.floor(self.__patch_id/41)
 		c = self.__patch_id % 41
 		return [r,c]
 
+
+	'''
+	Toggles the isSettlement boolean value
+	'''
 	def toggleSettlement(self):
 		self.__isSettlement =  not self.__isSettlement
 
+	'''
+	Toggles the isField boolean value
+	'''
 	def toggleField(self):
 		self.__isField = not self.__isField
 
+	'''
+	Toggles the isRiver boolean value
+	'''
 	def toggleRiver(self):
 		self.__isRiver = not self.__isRiver
 
+	'''
+	Toggles the isOwned boolean value
+	'''
 	def toggleOwned(self):
 		self.__isOwned = not self.__isOwned
 
+	'''
+	Changes the colour of the patch
+	'''
 	def changeColour(self, colour):
 		self.__colour = colour
 
@@ -54,7 +77,9 @@ class Patch:
 
 
 	class Field:
-
+		'''
+		Constructor method
+		'''
 		def __init__(self, field_id):
 			self.__field_id = field_id
 			self.__fertility = 0
@@ -73,15 +98,18 @@ class Patch:
 		def isHarvested(self):
 			return self.__harvested
 
+		'''
+		Toggles the isHarvested boolean value
+		'''
 		def toggleHarvested(self):
 			self.__harvested = not self.__harvested
 
+		'''
+		Allows a household to claim land owned by another household if that land has not been harvested for a certain amount of time (based on initial starting parameters)
+		'''
 		def fieldChangeover(self):
 			if(self.__harvested == True):
 				self.__years_fallow = 0
 			else:
 				self.__years_fallow += 1
 			return self.__years_fallow
-
-		def updateHousehold():
-			return 0
